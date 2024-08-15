9ed95050b88bcda6279cb879627ba686cdb1c173
rajatrjsharma <merajatsharma@outlook.com>
2024-08-16 03:14:08 +0530

    Update for allowed host in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,4 +2,9 @@ from .base import *
 
 
 DEBUG = False
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "13.127.133.231",
+    "ec2-13-127-133-231.ap-south-1.compute.amazonaws.com",
+]
